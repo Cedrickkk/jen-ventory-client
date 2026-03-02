3ba1beedece1b89d2a626471195b44453f0e0516
hulaanmo <inhog.cedrick@gmail.com>
2026-03-02 16:14:23 +0800

    feat: add tanstack query

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,8 +1,17 @@
 import { routeTree } from "@/routeTree.gen";
+import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
+
+const queryClient = new QueryClient();
 
 export const router = createRouter({
   routeTree,
+  context: {
+    queryClient,
+  },
+  defaultPreload: "intent",
+  defaultPreloadStaleTime: 0,
+  scrollRestoration: true,
 });
 
 declare module "@tanstack/react-router" {
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import QueryProvider from "./provider/query-provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryProvider>
+      <RouterProvider router={router} />
+    </QueryProvider>
   </StrictMode>,
 );
