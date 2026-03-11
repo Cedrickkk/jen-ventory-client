9135aed9b05c7e4b9d0199813ef82faa0669c040
hulaanmo <inhog.cedrick@gmail.com>
2026-03-11 13:39:17 +0800

    feat(routes): add head metadata for gcash and transactions routes

--- src/routes/_authenticated/gcash/index.tsx
+++ src/routes/_authenticated/gcash/index.tsx
@@ -1,9 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_authenticated/gcash/')({
+export const Route = createFileRoute("/_authenticated/gcash/")({
+  head: () => ({
+    meta: [
+      {
+        title: "GCash - JenVentory",
+      },
+    ],
+  }),
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_authenticated/gcash/"!</div>
+  return <div>Hello "/_authenticated/gcash/"!</div>;
 }
--- src/routes/_authenticated/transactions/index.tsx
+++ src/routes/_authenticated/transactions/index.tsx
@@ -1,9 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_authenticated/transactions/')({
+export const Route = createFileRoute("/_authenticated/transactions/")({
+  head: () => ({
+    meta: [
+      {
+        title: "GCash - JenVentory",
+      },
+    ],
+  }),
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_authenticated/transactions/"!</div>
+  return <div>Hello "/_authenticated/transactions/"!</div>;
 }
