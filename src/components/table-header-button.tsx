f0edaa99307b659f7248e19aaf08c098e81cb538
hulaanmo <inhog.cedrick@gmail.com>
2026-04-16 11:03:36 +0800

    refactor(table-header-button): simplify component structure by removing unnecessary div wrapper

--- src/components/table-header-button.tsx
+++ src/components/table-header-button.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import type { Column } from "@tanstack/react-table";
 import { ChevronsUpDown } from "lucide-react";
 import type { ReactNode } from "react";
@@ -13,18 +12,15 @@ type TableHeaderButtonProps<TData> = {
 export function TableHeaderButton<TData>({
   column,
   children,
-  className,
 }: TableHeaderButtonProps<TData>) {
   return (
-    <div className={cn("flex items-start justify-start py-3", className)}>
-      <Button
-        variant="ghost"
-        className="text-muted-foreground w-fit cursor-pointer rounded-sm p-2 text-left hover:bg-white/10"
-        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-      >
-        {children}
-        <ChevronsUpDown className="size-3" />
-      </Button>
-    </div>
+    <Button
+      variant="ghost"
+      className="text-muted-foreground my-3 w-full cursor-pointer justify-start rounded-sm py-3 text-left hover:bg-white/10"
+      onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+    >
+      {children}
+      <ChevronsUpDown className="size-3" />
+    </Button>
   );
 }
